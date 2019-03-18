88341af1b11e7d8065018a336c01a1c7fd4eb97b
Thorsten <t.luenborg@googlemail.com>
2019-03-18 23:50:14 +0100

    simplify TargetContainer implementation

--- src/components/TargetContainer.js
+++ src/components/TargetContainer.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 export default Vue.extend({
+  // as an abstract component, it doesn't appear in
+  // the $parent chain of components.
+  // which means the next parent of any component rendered inside of this oen
+  // will be the parent from which is was portal'd
   abstract: true,
   name: 'PortalOutlet',
   props: ['nodes', 'tag'],
   data: () => ({
-    updatedNodes: null,
+    updatedNodes: this.nodes,
   }),
   render(h) {
-    return h(
-      this.tag || 'DIV',
-      this.updatedNodes ? this.updatedNodes() : this.nodes()
-    )
+    return h(this.tag || 'DIV', this.updatedNodes())
   },
 })
