34447edee0acc09c2245fe46d185d63a7cd0372b
Thorsten <t.luenborg@googlemail.com>
2019-04-14 20:08:26 +0200

    demo: make it work

--- demo/App.vue
+++ demo/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <img width="25%" src="./assets/logo.png" /> <br />
-    <button @click="showPortal = !showPortal">Toggle Render</button>
+    <button @click="toggle">Toggle Render</button>
     <button @click="disablePortal = !disablePortal">Toggle disabled</button>
     <portal v-if="showPortal" :disabled="disablePortal">
-      <p>Test</p>
-      <Test />
+      <transition name="fade" appear @afterLeave="finishExit">
+        <div v-if="showContent">
+          <p>Test</p>
+          <Test />
+        </div>
+      </transition>
     </portal>
     <hr />
   </div>
@@ -20,8 +24,23 @@ export default {
   },
   data: () => ({
     showPortal: true,
+    showContent: true,
     disablePortal: false,
   }),
+  methods: {
+    toggle() {
+      if (!this.showPortal) {
+        this.showContent = true
+        this.showPortal = true
+      } else {
+        this.showContent = false
+      }
+    },
+    finishExit() {
+      console.log('finished!')
+      this.showPortal = false
+    },
+  },
 }
 </script>
 
@@ -33,5 +52,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import SimplePortal from '../src'
 
+Vue.use(SimplePortal)
 Vue.config.productionTip = false
 
 new Vue({
