3ad31c02af44ad1236d3c605c4d34d7298c9e65d
Thorsten <t.luenborg@googlemail.com>
2019-04-24 16:56:51 +0200

    first e2e test attempt

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,14 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
+  it('renders initial page', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js App')
+  })
+
+  it('mounts a bare portal correctly', () => {
+    cy.visit('/')
+    const selector = cy.window().PortalSelector
+    cy.get(selector).should.have.descendants('[data-test="test-header"]')
   })
 })
