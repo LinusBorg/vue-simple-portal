940b285ea68d4c2390e980d530b50d892d7e7719
Christian Kienle <christian.kienle@sap.com>
2019-04-28 19:44:39 +0200

    fix: Bundles nanoid along with VSP and fixes a typo

--- src/components/TargetContainer.js
+++ src/components/TargetContainer.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 export default Vue.extend({
   // as an abstract component, it doesn't appear in
   // the $parent chain of components.
-  // which means the next parent of any component rendered inside of this oen
+  // which means the next parent of any component rendered inside of this one
   // will be the parent from which is was portal'd
   abstract: true,
   name: 'PortalOutlet',
